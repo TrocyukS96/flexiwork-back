3e982fa25ff7068be638f3c2be97d2490a52e07f
TrocyukS96 <trotzuk.stanislav@gmail.com>
2024-08-11 08:27:53 +0300

    added test endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { DbService } from './db/db.service';
+import { Post as PostModel } from '@prisma/client';
 
 class HelloWorldDto {
   message:string
+}
+
+class PostDto {
+  title:string
+  description:string
+}
+
+class GetPostsDto{
+  posts:PostDto[]
+}
+
+class CreatePostDto {
+  title:string
+  description:string
 }
 
 
@@ -11,10 +26,22 @@ class HelloWorldDto {
 export class AppController {
   constructor(private readonly appService: AppService, private dbService:DbService) {}
 
+
+
   @Get()
-  async getHello(): Promise<HelloWorldDto> {
-    const users =await this.dbService.user.findMany({})
-    console.log('users', users)
-    return {message:this.appService.getHello()}
+  async getPosts(): Promise<GetPostsDto> {
+    const posts =await this.appService.getPosts()
+    console.log('posts', posts)
+    return posts
+  }
+
+  @Post()
+  async createPost(@Body() createPostDto: CreatePostDto) {
+    return await this.appService.createPost(createPostDto);
+  }
+
+  @Delete(':id')
+  async deletePost(@Param('id') id: string): Promise<PostModel > {
+    return this.appService.deletePost({ id: Number(id) });
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Post, Prisma } from '@prisma/client';
+import { DbService } from './db/db.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private dbService: DbService) {}
+
+ async getPosts(): Promise<{posts:Post[]}> {
+  const posts =await this.dbService.post.findMany({
+    });
+    return {posts}
   }
+
+  async createPost(data: {
+    title:string,
+    description:string
+  }): Promise<Post> {
+    const response  =await this.dbService.post.create({
+      data,
+    });
+
+    return response
+  }
+
+  async deletePost(where: Prisma.PostWhereUniqueInput): Promise<Post> {
+    const response = await this.dbService.post.delete({
+      where,
+    });
+
+    return response
+  }
+
 }
