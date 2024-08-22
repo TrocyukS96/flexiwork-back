4869e4c0dc239692c3134c1e2e3e0b07aae8a6d6
TrocyukS96 <trotzuk.stanislav@gmail.com>
2024-08-22 08:45:00 +0300

    added fix to swager config

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app,config)
 
-  SwaggerModule.setup('docs', app, document)
+  SwaggerModule.setup('api/docs', app, document)
 
   await app.listen(4200, '0.0.0.0');
 }
